38219a6e52dc93e29ff6c101606c17aa918061d7
Kevin Chappell <kevin.b.chappell@gmail.com>
2016-06-06 11:13:17 +0100

    update package.json and build-icons.sh

--- build-icons.sh
+++ build-icons.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-#
 
 svgo -f src/icons -o src/icons/optimized --config="{\"plugins\":[{\"cleanupAttrs\":true},{\"removeDimensions\":true},{\"removeTitle\":true},{\"removeUselessDefs\":true},{\"mergePaths\":true},{\"removeStyleElement\":true},{\"removeNonInheritableGroupAttrs\":true},{\"removeAttrs\":{\"attrs\":[\"fill\",\"style\"]}}]}"
 svg-sprite -s --symbol-dest="dist/" src/icons/optimized/*.svg
