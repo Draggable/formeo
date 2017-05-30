3b8dcb2c54dc7e20c68399c4fa524ced28ff5ab2
Kevin Chappell <kevin.b.chappell@gmail.com>
2017-05-30 13:09:02 +0100

    Improve semver regex, Add full PR gitlog to release description and changelog

--- tools/tag.js
+++ tools/tag.js
@@ -12,55 +12,63 @@ import {exec} from 'child_process';
 import json from 'json-update';
 
 /**
+ * Capitalizes a string
+ * @param  {String} str uncapitalized string
+ * @return {String} str capitalized string
+ */
+const capitalize = str => {
+  return str.replace(/\b\w/g, function(m) {
+      return m.toUpperCase();
+    });
+};
+
+/**
  * Updates README AND CHANGELOG
  * @param  {Object} version current and new version
+ * @param  {String} gitLog
  * @return {void}
  */
-function updateMd(version) {
+function updateMd(version, gitLog) {
   const lastLog = fs.readFileSync('./CHANGELOG.md', 'utf8').split('\n')[2];
-  return exec('git log -1 HEAD --pretty=format:%s', function(err, gitLog) {
-    gitLog = gitLog.replace(/\(#(\d+)\)/g, `[#$1](${pkg.repository.url}/pulls/$1)`);
-    const changes = [
-      {
-        files: 'CHANGELOG.md',
-        from: lastLog,
-        to: `- v${version.new} - ${gitLog}\n${lastLog}`
-      }, {
-        files: 'README.md',
-        from: new RegExp(`^${pkg.name} v\\d.\\d.\\d`),
-        to: `${pkg.name} v${version.new}`
-      }
-    ];
+  const changes = [
+    {
+      files: 'CHANGELOG.md',
+      from: lastLog,
+      to: `- v${version} - ${gitLog}\n${lastLog}`
+    }, {
+      files: 'README.md',
+      from: new RegExp(`^(?:${pkg.name}|${capitalize(pkg.name)}) v(?:0|[1-9]\\d*).(?:0|[1-9]\\d*).(?:0|[1-9]\\d*)`),
+      to: `${capitalize(pkg.name)} v${version}`
+    }
+  ];
 
-    changes.forEach(change =>
-      replace(change)
-        .then(changedFiles => {
-          console.log('Modified files:', changedFiles.join(', '));
-        })
-        .catch(error => {
-          console.error('Error occurred:', error);
-        })
-    );
-  });
+  changes.forEach(change =>
+    replace(change)
+      .then(changedFiles => {
+        console.log('Modified files:', changedFiles.join(', '));
+      })
+      .catch(error => {
+        console.error('Error occurred:', error);
+      })
+  );
 }
 
 /**
  * Build, push, tag and npm publish
- * @param  {String} version [description]
+ * @param  {String} version
+ * @param  {String} description
  * @return {Promise} exec
  */
-function release(version) {
+function release(version, description) {
   const commands = [
     'npm run build',
     'git add --all',
-    `git commit -am "v${version}" -m `,
+    `git commit -am "v${version}" -m "${description}"`,
     `git tag v${version}`,
     'git push upstream master --tags',
     'npm publish'
   ];
 
-  console.log('commands => ', commands);
-  return;
   return exec(commands.join(' && '), (err, stdout) => {
     if (!err) {
       console.log(stdout);
@@ -82,10 +90,13 @@ async function tag() {
     new: semver.inc(pkg.version, releaseType)
   };
 
-  await updateMd(version);
-  await json.update('bower.json', {version: version.new});
-  await json.update('package.json', {version: version.new});
-  return release(version.new);
+  return exec('git log -1 HEAD --pretty=format:%s%n%b', async function(err, gitLog) {
+    gitLog = gitLog.replace(/\(#(\d+)\)/g, `[#$1](${pkg.repository.url}/pulls/$1)`).trim();
+    updateMd(version.new, gitLog);
+    await json.update('bower.json', {version: version.new});
+    await json.update('package.json', {version: version.new});
+    return release(version.new, gitLog);
+  });
 }
 
 export default tag;
