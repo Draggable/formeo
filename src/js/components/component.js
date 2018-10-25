35f34d5e3c6d259c45089b4e59991e14d2a85258
Kevin Chappell <kevin.b.chappell@gmail.com>
2018-10-25 12:51:48 -0400

    Ensure mouseevents are added after render

--- src/js/components/component.js
+++ src/js/components/component.js
@@ -104,23 +104,30 @@ export default class Component extends Data {
    * @return {Object} element config object
    */
   getActionButtons() {
-    let expandSize
-    const hoverClassname = `hovering-${this.name}`
     return {
       className: `${this.name}-actions group-actions`,
       action: {
         onRender: elem => {
           const buttons = elem.getElementsByTagName('button')
           const btnSize = parseInt(dom.getStyle(buttons[0], 'width'))
-          expandSize = `${buttons.length * btnSize + 1}px`
-        },
-        mouseenter: ({ target }) => {
-          this.dom.classList.add(hoverClassname)
-          target.style[this.name === 'row' ? 'height' : 'width'] = expandSize
-        },
-        mouseleave: ({ target }) => {
-          this.dom.classList.remove(hoverClassname)
-          target.removeAttribute('style')
+          const expandSize = `${buttons.length * btnSize + 1}px`
+          const hoverClassname = `hovering-${this.name}`
+          elem.addEventListener(
+            'mouseenter',
+            ({ target }) => {
+              this.dom.classList.add(hoverClassname)
+              target.style[this.name === 'row' ? 'height' : 'width'] = expandSize
+            },
+            false
+          )
+          elem.addEventListener(
+            'mouseleave',
+            ({ target }) => {
+              this.dom.classList.remove(hoverClassname)
+              target.removeAttribute('style')
+            },
+            false
+          )
         },
       },
       children: {
