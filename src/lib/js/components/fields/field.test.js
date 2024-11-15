2825f54a74978af4e0408b64ee6d7f4d4807922e
Kevin Chappell <kevin.b.chappell@gmail.com>
2024-11-15 15:22:41 -0800

    chore: remove dead code

--- src/lib/js/components/fields/field.test.js
+++ src/lib/js/components/fields/field.test.js
@@ -1,5 +1,4 @@
 import { describe, it, beforeEach, mock } from 'node:test'
-import assert from 'node:assert/strict'
 import '../../../../../tools/__mocks__/ResizeObserver.js'
 import Field from './field.js'
 
@@ -15,13 +14,5 @@ describe('Field', () => {
 
   it('should have data property matching snapshot', t => {
     t.assert.snapshot(field.data)
-  })
-
-  it('should call onRender from constructor', t => {
-    // t.mock.method(field, 'onRender')
-    const call = field.onRender.mock.calls[0]
-    console.log(call)
-    // assert.deepEqual(call.arguments, [3]);
-    // assert.ok(spy.calledWith(field.dom))
   })
 })
