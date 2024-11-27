0d6ec6a8abd7797954866a6188af4ac55f1d96bc
Kevin Chappell <kevin@chappell.dev>
2024-11-27 09:46:35 -0800

    feat: add ability to test formData in demo

--- src/demo/js/actionButtons.js
+++ src/demo/js/actionButtons.js
@@ -17,13 +17,32 @@ const editorActions = (editor, renderer) => ({
     window.sessionStorage.removeItem('formeo-formData')
     window.location.reload()
   },
+  testData: () => {
+    document.getElementById('submit-popover').addEventListener('click', () => {
+      const textarea = document.querySelector('#formData-popover textarea')
+      editor.formData = textarea.value
+      textarea.value = ''
+      textarea.closest('[popover]').hidePopover()
+    })
+  },
 })
+
+const buttonIdAttrsMap = {
+  testData: { popovertarget: 'formData-popover' },
+}
+const getButtonAttrs = id => {
+  const attrs = buttonIdAttrsMap[id] || {}
+  return { id, type: 'button', ...attrs }
+}
 
 export const editorButtons = (editor, renderer) => {
   const buttonActions = editorActions(editor, renderer)
   const buttons = Object.entries(buttonActions).map(([id, cb]) => {
-    const attrs = { id, type: 'button' }
+    const attrs = getButtonAttrs(id)
     const button = Object.assign(document.createElement('button'), attrs)
+    for (const [key, value] of Object.entries(attrs)) {
+      button.setAttribute(key, value)
+    }
     const buttonText = document.createTextNode(startCase(id))
     button.appendChild(buttonText)
     button.addEventListener('click', cb, false)
