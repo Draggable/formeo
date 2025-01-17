b9c44de193ad9d73fff423151992700d70e0cd5a
Kevin Chappell <kevin@chappell.dev>
2025-01-17 17:38:47 +0000

    wip: setup playwright

---
+++ tests/conditions.formData.js
@@ -0,0 +1,147 @@
+export const conditionalFields = {
+  id: 'a95d0c69',
+  stages: {
+    ff163056: {
+      conditions: [
+        {
+          if: [
+            {
+              source: '',
+              sourceProperty: '',
+              comparison: '',
+              target: '',
+              targetProperty: '',
+            },
+          ],
+          then: [
+            {
+              target: '',
+              targetProperty: '',
+              assignment: '',
+              value: '',
+            },
+          ],
+        },
+      ],
+      children: ['c5060f33', '0865a5cf'],
+      id: 'ff163056',
+    },
+  },
+  rows: {
+    '0865a5cf': {
+      config: {
+        fieldset: false,
+        legend: '',
+        inputGroup: false,
+      },
+      children: ['f0002d5c', '943b057f', 'fed0e1c4'],
+      className: ['formeo-row'],
+      id: '0865a5cf',
+    },
+    c5060f33: {
+      config: {
+        fieldset: false,
+        legend: '',
+        inputGroup: false,
+      },
+      children: ['2474fbd2'],
+      className: ['formeo-row'],
+      id: 'c5060f33',
+    },
+  },
+  columns: {
+    f0002d5c: {
+      config: {
+        width: '33.3%',
+      },
+      children: ['056be3d7'],
+      className: ['formeo-column'],
+      id: 'f0002d5c',
+    },
+    fed0e1c4: {
+      config: {
+        width: '33.3%',
+      },
+      children: ['7e3298ba'],
+      className: ['formeo-column'],
+      id: 'fed0e1c4',
+    },
+    '943b057f': {
+      config: {
+        width: '33.3%',
+      },
+      children: ['a3baf870'],
+      className: ['formeo-column'],
+      id: '943b057f',
+    },
+    '2474fbd2': {
+      config: {
+        width: '100%',
+      },
+      children: ['6c67ea4a'],
+      className: ['formeo-column'],
+      id: '2474fbd2',
+    },
+  },
+  fields: {
+    '056be3d7': {
+      tag: 'input',
+      attrs: {
+        required: false,
+        type: 'text',
+        className: '',
+      },
+      config: {
+        label: 'Input One',
+        controlId: 'text-input',
+      },
+      id: '056be3d7',
+    },
+    '7e3298ba': {
+      tag: 'input',
+      attrs: {
+        required: false,
+        type: 'text',
+        className: '',
+      },
+      config: {
+        label: 'Input Three',
+        controlId: 'text-input',
+      },
+      id: '7e3298ba',
+    },
+    a3baf870: {
+      tag: 'input',
+      attrs: {
+        required: false,
+        type: 'text',
+        className: '',
+      },
+      config: {
+        label: 'Input Two',
+        controlId: 'text-input',
+      },
+      id: 'a3baf870',
+    },
+    '6c67ea4a': {
+      tag: 'input',
+      attrs: {
+        type: 'checkbox',
+        required: false,
+      },
+      config: {
+        label: 'Checkbox Group',
+        disabledAttrs: ['type'],
+        controlId: 'checkbox',
+      },
+      options: [
+        {
+          label: 'Checkbox 1',
+          value: 'checkbox-1',
+          checked: false,
+        },
+      ],
+      id: '6c67ea4a',
+    },
+  },
+}
